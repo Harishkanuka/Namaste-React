e1bbc0a2d1161d3c6f831abb30d730410eaae46a
Your Name <kanukaharish@gmail.com>
2023-09-01 08:27:28 +0530

    Episode-01(Inception)

---
+++ 01.Inception/App.js
@@ -0,0 +1,12 @@
+//If We have to do nested elements using core React
+//If we have pass another createelement as third argument (i.e Chidren)
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }),
+    [
+        React.createElement("h1", {}, "Im an h1 tag"),
+        React.createElement("h2", {}, "Im an h2 tag"),
+    ],
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
